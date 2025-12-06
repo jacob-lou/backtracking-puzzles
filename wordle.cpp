10449286bc644a5de9f7b7842e06fd859461ca28
hao lou <jacoblou@usc.edu>
2025-12-06 07:09:32 +0000

    Update hw5 wordle

--- wordle.cpp
+++ wordle.cpp
@@ -14,12 +14,12 @@ using namespace std;
 
 // Add prototypes of helper functions here
 void wordleHelper(
-    const std::string& pattern,          
-    std::string& cur,                    
-    size_t idx,                          
-    std::string floatingLeft,            
-    const std::set<std::string>& dict,   
-    std::set<std::string>& results       
+    const std::string& pattern,
+    std::string& cur,
+    size_t idx,
+    std::string floatingLeft,
+    const std::set<std::string>& dict,
+    std::set<std::string>& results     
 );
 
 // Definition of primary wordle function
@@ -34,6 +34,7 @@ std::set<std::string> wordle(
       return results;
   }
   std::string cur=in;
+  
   wordleHelper(in, cur, 0, floating, dict, results);
   return results;
 }
@@ -47,62 +48,83 @@ void wordleHelper(
     const std::set<std::string>& dict,
     std::set<std::string>& results)
 {
-  size_t n=pattern.size();
+  size_t n = cur.size();
+
+  size_t emptySlots = 0;
+  for(size_t i = idx; i < n; ++i){
+    if(cur[i] == '-') {
+        emptySlots++;
+    }
+  } 
+    
+  if(floatingLeft.size()>emptySlots){
+    return;
+  }
+
   if(idx==n){
-    if(floatingLeft.empty()){
-      if(dict.find(cur)!=dict.end()){
-        results.insert(cur);
-      }
+    if(floatingLeft.empty() && dict.find(cur) != dict.end()){
+      results.insert(cur);
     }
     return;
   }
 
-  size_t remainingSlots = n - idx;
-  if(floatingLeft.size()>remainingSlots){
-    return;
-  }
-  char p = pattern[idx];
-
-  if(p!= '-'){
-    cur[idx]= p;
-
-    size_t pos = floatingLeft.find(p);
-    if(pos != std::string::npos){
-      floatingLeft.erase(pos,1);
-    }
-
-    wordleHelper(pattern, cur, idx+1,floatingLeft,dict,results);
+  if(pattern[idx] != '-'){
+    wordleHelper(pattern, cur, idx + 1, floatingLeft, dict, results);
   }else{
-    if(floatingLeft.size() == remainingSlots){
-      for(size_t i = 0; i < floatingLeft.size(); ++i){
-        char ch = floatingLeft[i];
+    for(char ch = 'a'; ch<='z'; ++ch){
         cur[idx] = ch;
 
-        std::string newFloating = floatingLeft;
-        newFloating.erase(i, 1); 
-
-        wordleHelper(pattern, cur, idx + 1, newFloating, dict, results);
-      }
-    }
-    else{
-      for(char ch = 'a'; ch<='z'; ++ch){
-        cur[idx] = ch;
-
-        std::string newFloating = floatingLeft;
-        size_t pos = newFloating.find(ch);
-        if(pos!= std::string::npos){
-          newFloating.erase(pos,1);
+        std::string nextFloating=floatingLeft;
+        size_t pos =nextFloating.find(ch);
+        if(pos != std::string::npos){
+          nextFloating.erase(pos,1);
         }
 
-        size_t remainingSlotsNext = n-(idx+1);
-
-        if(newFloating.size()>remainingSlotsNext){
-          continue;
-        }
-
-        wordleHelper(pattern, cur, idx+1, newFloating, dict, results);    
+        wordleHelper(pattern, cur, idx + 1, nextFloating, dict, results);
       }
-    }
+    cur[idx] = '-';
   }
+
+
+
+  // char p = pattern[idx];
+
+  // if(p!= '-'){
+  //   cur[idx]= p;
+
+  //   size_t pos = floatingLeft.find(p);
+  //   if(pos != std::string::npos){
+  //     std::string nextFloating = floatingLeft;
+  //     nextFloating.erase(pos,1);
+  //     wordleHelper(pattern,cur,idx+1,nextFloating,dict,results);
+  //   }else{
+  //     wordleHelper(pattern,cur,idx+1,floatingLeft,dict,results);
+  //   }
+  // }else{
+  //   if(floatingLeft.size() == emptySlots){
+  //     for(size_t i = 0; i < floatingLeft.size(); i++){
+  //       char ch = floatingLeft[i];
+  //       cur[idx] = ch;
+
+  //       std:: string nextFloating = floatingLeft;
+  //       nextFloating.erase(i,1);
+
+  //       wordleHelper(pattern, cur, idx + 1, nextFloating, dict, results);
+  //     }
+  //   }
+  //   else{
+  //     for(char ch = 'a'; ch<='z'; ++ch){
+  //       cur[idx] = ch;
+
+  //       std::string nextFloating=floatingLeft;
+  //       size_t pos =nextFloating.find(ch);
+  //       if(pos != std::string::npos){
+  //         nextFloating.erase(pos,1);
+  //       }
+
+  //       wordleHelper(pattern, cur, idx + 1, nextFloating, dict, results);
+  //     }
+  //   }
+  // }
 
 }
